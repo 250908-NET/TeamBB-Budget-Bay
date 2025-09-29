aa4d937f542ea7789d7f302624c241cf92d4b3f5
Salvarez99 <stephenalvarez3@gmail.com>
2025-09-29 14:23:21 -0400

    Team repo set up

---
+++ BudgetBay.App/BudgetBay.Api/Program.cs
@@ -0,0 +1,29 @@
+using Serilog;
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+public partial class Program{};
