3bc7996e0c5293745bad80841bf8bfbf88261b66
matthewschade <mschadey612@gmail.com>
2025-09-30 14:22:29 -0400

    BidModel and Repo and DTO

---
+++ BudgetBay.App/BudgetBay.Api/Models/Bid.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BudgetBay.Models
+{
+    public class Bid 
+    {
+        [Key]
+        public int BidId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int BidderId { get; set; }
+        public Customer Bidder { get; set; }
+    }
+}
