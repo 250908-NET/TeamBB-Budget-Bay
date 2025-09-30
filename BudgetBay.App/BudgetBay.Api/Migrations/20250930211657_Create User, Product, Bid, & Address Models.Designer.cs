6cda434d57623d2506730fc82f4678bb88bf4612
Husan Nizom <husan.dev@h-n.tech>
2025-09-30 17:19:49 -0400

    feat: migrate new models

--- BudgetBay.App/BudgetBay.Api/Migrations/20250930211657_Create User, Product, Bid, & Address Models.Designer.cs
+++ BudgetBay.App/BudgetBay.Api/Migrations/20250930211657_Create User, Product, Bid, & Address Models.Designer.cs
@@ -4,6 +4,7 @@ using BudgetBay.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BudgetBay.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250930211657_Create User, Product, Bid, & Address Models")]
+    partial class CreateUserProductBidAddressModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
