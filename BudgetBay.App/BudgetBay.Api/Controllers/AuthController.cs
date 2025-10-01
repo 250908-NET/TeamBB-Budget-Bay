6e817d42644166b95efd99a6ca6c344c35a7c097
Husan Nizom <husan.dev@h-n.tech>
2025-10-01 13:39:58 -0400

    Implement auth endpoints and protect weather endpoint for auth testing

--- BudgetBay.App/BudgetBay.Api/Controllers/AuthController.cs
+++ BudgetBay.App/BudgetBay.Api/Controllers/AuthController.cs
@@ -9,7 +9,7 @@ using BudgetBay.DTOs;
 namespace BudgetBay.Controllers
 {
     [ApiController]
-    [Route("api/auth/[controller]")]
+    [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
@@ -22,13 +22,37 @@ namespace BudgetBay.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newUser = await _authService.Register(registerUserDto);
+
+            if (newUser is null)
+            {
+                return BadRequest("A user with this email or username already exists.");
+            }
+
+            return Created();
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginUserDto loginUserDto)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var token = await _authService.Login(loginUserDto);
+
+            if (token == null)
+            {
+                return Unauthorized("Invalid credentials.");
+            }
+
+            return Ok(token); // maybe create a TokenDto 
         }
     }
 }
