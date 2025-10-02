450ec53f158cde5a3024100cbc293ca9fd5a42e0
Husan Nizom <husan.dev@h-n.tech>
2025-10-02 18:12:38 -0400

    setup a router

--- BudgetBay.App/BudgetBay.Client/src/App.jsx
+++ BudgetBay.App/BudgetBay.Client/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
 
   return (
-    <>
-      <h1>BudgetBay</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
 
