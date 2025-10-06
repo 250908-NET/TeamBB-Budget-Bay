eb498902c28c4fde0732ccf8a501fb8148ebcd78
Husan Nizom <husan.dev@h-n.tech>
2025-10-06 16:10:40 -0400

    allow cors from anywhere

--- BudgetBay.App/BudgetBay.Api/Program.cs
+++ BudgetBay.App/BudgetBay.Api/Program.cs
@@ -108,7 +108,7 @@ public class Program
         builder.Services.AddCors(options =>
         {
             options.AddPolicy("AllowSpecificOrigin",
-                builder => builder.WithOrigins("http://localhost:5173") // Your frontend URL
+                builder => builder.WithOrigins("*") // Your frontend URL
                                 .AllowAnyHeader()
                                 .AllowAnyMethod());
         });
