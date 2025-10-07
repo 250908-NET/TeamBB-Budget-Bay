f0bd33dbc9f5c9b76bbe5ae956f13f342f9e1893
LedyaBakloug <lbakloug@gmail.com>
2025-10-07 14:52:29 -0400

    header done for now

--- BudgetBay.App/BudgetBay.Client/src/components/layout/Header.jsx
+++ BudgetBay.App/BudgetBay.Client/src/components/layout/Header.jsx
@@ -1,59 +1,70 @@
-import React, { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { AuthContext } from "../../contexts/AuthContext";
-import logo from "../../assets/full-logo.png";
-import "./Header.css";
+import React, { useContext } from 'react';
+import { Link, useLocation , useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+import logo from '../../assets/full-logo.png';
+import './Header.css';
 
 const Header = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
-  const { user, logout } = useContext(AuthContext);
+    const location = useLocation();
+    const currentPath = location.pathname;
+    const navigate = useNavigate();
+    const { user, logout } = useContext(AuthContext);
 
-  const handleSignOut = () => {
-    logout();
-  };
+    const handleSignOut = () => {
+        logout();
+    };
 
-  return (
-    <header className="header">
-      <div>
-        <Link to="/">
-          <img src={logo} alt="BudgetBay Logo" className="logo" />
-        </Link>
-      </div>
-      <nav>
-        <ul className="nav-links">
-          {user ? (
-            <li>
-              <a onClick={handleSignOut}>Sign Out</a>
-            </li>
-          ) : (
-            <>
-              {currentPath === "/login" && (
-                <li>
-                  <Link to="/signup">Sign Up</Link>
-                </li>
-              )}
-              {currentPath === "/signup" && (
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-              )}
-              {currentPath !== "/login" && currentPath !== "/signup" && (
-                <>
-                  <li>
-                    <Link to="/login">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="/signup">Sign Up</Link>
-                  </li>
-                </>
-              )}
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className='header'>
+            <div>
+                <Link to="/">
+                    <img src={logo} alt="BudgetBay Logo" className="logo" />
+                </Link>
+            </div>
+            <nav>
+                <ul className='nav-links'>
+                    {user ? (
+                        <li>
+                           <button onClick={handleSignOut} className="nav-btn">
+                                Sign Out
+                            </button>
+                        </li>
+                    ) : (
+                        <>
+                            {currentPath === '/login' && (
+                                <li>
+                                      <button onClick={() => navigate('/signup')} className="nav-btn">
+                                        Sign Up
+                                    </button>
+                                </li>
+                            )}
+                            {currentPath === '/signup' && (
+                                <li>
+                                    <button onClick={() => navigate('/login')} className="nav-btn">
+                                            Login
+                                        </button>
+                                </li>
+                            )}
+                            {currentPath !== '/login' && currentPath !== '/signup' && (
+                                <>
+                                    <li>
+                                        <button onClick={() => navigate('/login')} className="nav-btn">
+                                            Login
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button onClick={() => navigate('/signup')} className="nav-btn">
+                                            Sign Up
+                                        </button>
+                                    </li>
+                                </>
+                            )}
+                        </>
+                    )}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
