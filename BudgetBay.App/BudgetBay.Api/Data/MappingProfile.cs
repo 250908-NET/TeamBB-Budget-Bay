bf8c1ccc450fa58013847ebb5f67c1bfdba0b360
Husan Nizom <husan.dev@h-n.tech>
2025-10-08 18:54:30 -0400

    Update get product by id to return seller and bids as well

--- BudgetBay.App/BudgetBay.Api/Data/MappingProfile.cs
+++ BudgetBay.App/BudgetBay.Api/Data/MappingProfile.cs
@@ -18,10 +18,11 @@ namespace BudgetBay.Data
             CreateMap<Bid, UserBidDto>()
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Bidder.Username))
                 .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount));
-            
+
             CreateMap<Address, AddressDto>();
             CreateMap<Product, CreateProductDto>().ReverseMap();
             CreateMap<Product, UpdateProductDto>().ReverseMap().ForAllMembers(opt => opt.Condition((src, dest, srcValue) => srcValue != null));
+            CreateMap<Product, ProductDetailDto>();
         }
     }
 }
