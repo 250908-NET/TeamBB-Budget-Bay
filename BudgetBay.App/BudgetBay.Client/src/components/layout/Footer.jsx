cebc5489873f9eeffc30b3ce5f0b652d2bc50188
LedyaBakloug <lbakloug@gmail.com>
2025-10-09 09:57:05 -0400

    add footer

--- BudgetBay.App/BudgetBay.Client/src/components/layout/Footer.jsx
+++ BudgetBay.App/BudgetBay.Client/src/components/layout/Footer.jsx
@@ -9,12 +9,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-           <div>
+           <div className="footer-logo-section">
                 <Link to="/">
                     <img src={logo} alt="BudgetBay Logo" className="logo" />
                 </Link>
+                <div className="copyright">
+                    <p>&copy; {currentYear} Husankhuja Nizomkhujaev, Kush Gandhi, Ledya Bakloug, Matthew Schade, Matthew Sims, & Stephen Alvarez. All rights reserved.</p>
+                </div>
             </div>
-
       </div>
     </footer>
   );
