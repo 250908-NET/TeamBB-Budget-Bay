3055bf68d2458bf091acfd7afc4eb9c42c09305c
LedyaBakloug <lbakloug@gmail.com>
2025-10-10 10:36:18 -0400

    fix homePage style

--- BudgetBay.App/BudgetBay.Client/src/pages/HomePage/HomePage.jsx
+++ BudgetBay.App/BudgetBay.Client/src/pages/HomePage/HomePage.jsx
@@ -8,42 +8,40 @@ import { getAllProducts } from "../../services/apiClient";
 
 const HomePage = () => {
   const { token, logout } = useContext(AuthContext);
-  const [ products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-
-useEffect(() => {
-  const fetchProducts = async () => {
-    const data = await getAllProducts();
-    setProducts(data);
-  };
-  fetchProducts();
-}, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await getAllProducts();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
 
   return (
-    <div className={styles.homepageContainer}>
-        <div className={styles.headerBlock}>
-            
+    <main>
+      <div className={styles.homepageContainer}>
+        <div className={styles.headerBlock}></div>
+        {token ? (
+          <div>
+            <h1>Welcome Back!</h1>
+            <p>You are logged in.</p>
+          </div>
+        ) : (
+          <div>
+            <h1>Welcome to BudgetBay</h1>
+            <p>Please log in to manage your budget.</p>
+          </div>
+        )}
+        <div className={styles.searchBarContainer}>
+          <SearchBar />
         </div>
-      {token ? (
-        <div>
-          <h1>Welcome Back!</h1>
-          <p>You are logged in.</p>
+        <div className={styles.productListContainer}>
+          <CatalogProduct onHome={true} Products={products} />
         </div>
-      ) : (
-        <div>
-          <h1>Welcome to BudgetBay</h1>
-          <p>Please log in to manage your budget.</p>
-        </div>
-      )}
-      <div className={styles.searchBarContainer}>
-        <SearchBar />
       </div>
-      <div className={styles.productListContainer}>
-        <CatalogProduct onHome={true} Products={products}/>
-      </div>
-    </div>
+    </main>
   );
 };
-
 
 export default HomePage;
